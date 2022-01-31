1b95b0eb8588e849357ae729371ac10aecd8e707
NotDoudou <96118111+NotDoudou@users.noreply.github.com>
2022-01-31 14:54:03 +0100

    Init Xcode project

---
+++ glowing-enigma/glowing-enigma/glowing_enigmaApp.swift
@@ -0,0 +1,17 @@
+//
+//  glowing_enigmaApp.swift
+//  glowing-enigma
+//
+//  Created by goldorak on 31/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct glowing_enigmaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

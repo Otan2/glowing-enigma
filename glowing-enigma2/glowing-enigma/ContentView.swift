42b47382b39f432addad234ae9808626ba11faa8
Antonin Boulnois <antonin.boulnois@efrei.net>
2022-01-31 15:01:40 +0100

    change version Xcode

---
+++ glowing-enigma2/glowing-enigma/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  glowing-enigma
+//
+//  Created by Antonin Boulnois on 31/01/2022.
+//  Copyright © 2022 Antonin Boulnois. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}

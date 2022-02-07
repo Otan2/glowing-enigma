0fd21c0c628efd2cef7c980f6b2e8ad43930e054
NotDoudou <96118111+NotDoudou@users.noreply.github.com>
2022-02-07 17:03:49 +0100

    Add files via upload

--- ContentView.swift
+++ ContentView.swift
@@ -12,26 +12,85 @@ struct ContentView: View {
     
     @State private var showDetails = false
     
+    @State private var name = ""
+    //@State private var begin: Date
+    //@State private var end: Date
+    @State private var begin = ""
+    @State private var end = ""
+    @State private var theme = ""
+    @State private var location = ""
+    
+    @State var text = ""
+    let labels = ["Title", "Start", "End", "Theme", "Location"]
+
+    var body: some View {
+        VStack {
+            Button("Add event") {
+                showDetails.toggle()
+            }
+            if showDetails {
+
+
+            HStack {
+                VStack(alignment: .leading) {
+                    ForEach(labels, id: \.self) { label in
+                        Text(label)
+                            .frame(maxHeight: .infinity)
+                            .padding(.bottom, 4)
+                    }
+                }
+
+                VStack {
+                    ForEach(labels, id: \.self) { label in
+                        TextField(label, text: self.$text)
+                            .textFieldStyle(RoundedBorderTextFieldStyle())
+                    }
+                }
+                .padding(.leading)
+            }
+            .padding(.horizontal)
+            .fixedSize(horizontal: false, vertical: true)
+            }
+        }
+        
+        
+        
+        
+        
+        
+        
+    }
+
+    
+    /*
     var body: some View {
         
         VStack(alignment: .leading) {
-                    Button("Add event") {
-                        showDetails.toggle()
-                    }
-
-                    if showDetails {
-                        TextField("Title of the event", text: $name)
-                        TextField("Start", text: $begin)
-                        TextField("End", text: $end)
-                        TextField("Theme", text: theme)
-                        TextField("Location", text: location)
-                    }
+            HStack(alignment: .topLeading) {
+                Button("Add event") {
+                    showDetails.toggle()
                 }
-        
-        VStack {
+                
+                Button("X") {
+                    showDetails.toggle()
+                }
+                
+
+                if showDetails {
+                    TextField("Title of the event", text: $name)
+                    TextField("Start", text: $begin)
+                    TextField("End", text: $end)
+                    TextField("Theme", text: $theme)
+                    TextField("Location", text: $location)
+                    
+                    Text("Name: "+name)
+                }
+            }
+        }
+        /*VStack {
             block(name: "Samba", begin: "0", end: "1", theme: "Danse", location: "Paris")
             block(name: "Samba", begin: "10", end: "11", theme: "Danse", location: "Paris")
-        }
+        }*/
     }
     
     var rectangle: some View {
@@ -40,7 +99,7 @@ struct ContentView: View {
             .fill(Color(red: 200, green: 200, blue: 255))
             //.stroke(Color.green)
             .frame(width: 250, height: 30)
-    }
+    }*/
 }
 
 struct ContentView_Previews: PreviewProvider {
